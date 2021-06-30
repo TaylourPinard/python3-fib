faa4c96d2d5e11c1a1414c8bb2df96ee5dc3094e
jayluvsdivn <jayluvsdivn@gmail.com>
2021-06-30 16:02:32 -0700

    adds input sanitization

--- fib.py
+++ fib.py
@@ -1,10 +1,16 @@
-bound = int(input("What number would you like to print the fib sequence up to?"))
-x = 0
-y = 1
-z = 1
-print("0")
-while(y <= bound):
-  print(y)
-  z = x + y
-  x = y
-  y = z
+while True:
+  try:
+    bound = int(input("What number would you like to print the fib sequence up to?"))
+  except:
+    print("Please only input a whole number")
+    continue
+  x = 0
+  y = 1
+  z = 1
+  print("0")
+  while(y <= bound):
+    print(y)
+    z = x + y
+    x = y
+    y = z
+  break 
